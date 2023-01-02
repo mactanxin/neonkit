7dceb251b54a5058685349e1505bdc44b89f4600
Xin Tan <mactanxin@gmail.com>
2023-01-02 10:23:57 +0800

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: '@neonkit',
+      filename: '@neonkit-example',
+    }
+  },
+  rollupOptions: {
+    external: ['vue'],
+    output: {
+      globals: {
+        vue: 'Vue'
+      }
+    }
+  }
+})
